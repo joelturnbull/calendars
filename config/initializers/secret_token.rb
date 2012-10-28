bb3ace9321ca6d5bf9a85209516dc7811f1db47f
Joel Turnbull <joelbywan@gmail.com>
2012-10-27 20:47:11 -0400

    initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calendars::Application.config.secret_token = 'e2e6f15538a40a73d0805ec5d5720c0f1f1108fc15fea2c4b4c8d2bd727a872177903b50ab65c500f5c4e59d15b856f4a8d219dc65b0ee6f75fc0e28262dd14b'
