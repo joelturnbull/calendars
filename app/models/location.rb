8afb8b498ec5e0de6622c3498056ce82df6644bb
Joel Turnbull <joelbywan@gmail.com>
2012-10-28 17:43:52 -0400

    locations can publish

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,10 @@
 class Location < ActiveRecord::Base
   has_many :events
   attr_accessible :name
+  
+  def publish
+    cal = RiCal::Component::Calendar.new
+    events.each { |event| cal.events << RiCal.parse_string(event.ics)[0] }
+    cal.to_s
+  end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,19 +1,28 @@
 require 'spec_helper'
 
 describe Location do
-  Given(:location) { Location.new(name:'MOTR') }
+  Given(:location) { Location.new(name:'The Comet') }
 
   context "is named" do
-    Then { location.name.should == 'MOTR' }
+    Then { location.name.should == 'The Comet' }
   end
-  context "has many events" do
-    Given(:event1) { Event.create(location:location,ics:ics) }
-    Given(:event2) { Event.create(location:location,ics:ics) }
-    Then { location.events.should include(event1) }
-    Then { location.events.should include(event2) }
+
+  context "events" do
+    Given!(:event1) { Event.create(location:location,ics:ics) }
+    Given!(:event2) { Event.create(location:location,ics:ics) }
+
+    context "has many" do
+      Then { location.events.should include(event1) }
+      Then { location.events.should include(event2) }
+    end
+
+    context "publishes collected events" do
+      When(:rical) { RiCal.parse_string(location.publish) }
+      Then { rical[0].events.count.should == 2 }
+    end
   end
 end
 
 def ics
-  "BEGIN:VCALENDAR\nVERSION:2.0\nMETHOD:PUBLISH\nX-WR-TIMEZONE:US/Eastern\nPRODID:-//CincyMusic.com//EN\nCALSCALE:GREGORIAN\nX-WR-CALNAME:CincyMusic.com\nBEGIN:VTIMEZONE\nTZID:US/Eastern\nBEGIN:STANDARD\nDTSTART:16011104T020000\nRRULE:FREQ=YEARLY;BYDAY=1SU;BYMONTH=11\nTZOFFSETFROM:-0400\nTZOFFSETTO:-0500\nEND:STANDARD\nBEGIN:DAYLIGHT\nDTSTART:16010311T020000\nRRULE:FREQ=YEARLY;BYDAY=2SU;BYMONTH=3\nTZOFFSETFROM:-0500\nTZOFFSETTO:-0400\nEND:DAYLIGHT\nEND:VTIMEZONE\nBEGIN:VEVENT\nSEQUENCE:1\nDESCRIPTION:Tweens\nURL;VALUE=URI:http://cincymusic.com/shows/2012/10/tweens2\nSUMMARY:Tweens\nLOCATION:The Comet\\n4579 Hamilton Avenue\\nCincinnati, Cincinnati 45223\nUID:b9d06666d7172070176d8a49e847304e@cincymusic.com\nDTSTART;TZID=US/Eastern:20121030T220000\nDTSTAMP:20121027T143347\nDTEND;TZID=US/Eastern:20121031T010000\nEND:VEVENT\nEND:VCALENDAR\n"
+  "BEGIN:VEVENT\nSEQUENCE:1\nDESCRIPTION:Tweens\nURL;VALUE=URI:http://cincymusic.com/shows/2012/10/tweens2\nSUMMARY:Tweens\nLOCATION:The Comet\\n4579 Hamilton Avenue\\nCincinnati, Cincinnati 45223\nUID:b9d06666d7172070176d8a49e847304e@cincymusic.com\nDTSTART;TZID=US/Eastern:20121030T220000\nDTSTAMP:20121027T143347\nDTEND;TZID=US/Eastern:20121031T010000\nEND:VEVENT\n"
 end
