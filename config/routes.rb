c3cdcc47ee0e75b64bf2e1259a4ac00115ce7b92
Joel Turnbull <joelbywan@gmail.com>
2012-11-19 23:39:29 -0500

    pluralization issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Calendars::Application.routes.draw do
 
-  resources :location
+  resources :locations
   root :to => 'locations#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
